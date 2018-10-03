9edac74fd277c58b9fa0adcc42fd2ac692b20e1b
mkshkmrgpt <mkshkmrgpt@gmail.com>
2018-10-03 20:35:50 +0530

    add intergration test cases

--- src/test/kotlin/com/mukesh/webcrawler/WebCrawlerApplicationTests.kt
+++ src/test/kotlin/com/mukesh/webcrawler/WebCrawlerApplicationTests.kt
@@ -1,16 +1,34 @@
 package com.mukesh.webcrawler
 
+import junit.framework.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpStatus
 import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class WebCrawlerApplicationTests {
 
+    @Autowired
+    lateinit var restTemplate: TestRestTemplate
+
     @Test
-    fun contextLoads() {
+    fun whenCalledShouldReturnJsonData() {
+        val result = restTemplate.getForEntity("/crawl?site=https://www.google.com",
+                CrawlInformation::class.java)
+        assertNotNull(result)
+        assertEquals(HttpStatus.OK, result.statusCode)
     }
 
+    @Test
+    fun whenInvalidURLIsProvidedShouldReturnErrorMessage() {
+        val result = restTemplate.getForEntity("/crawl?site=hps://www.google.com",
+                CrawlInformation::class.java)
+        assertNotNull(result)
+        assertEquals(HttpStatus.BAD_REQUEST, result.statusCode)
+    }
 }
