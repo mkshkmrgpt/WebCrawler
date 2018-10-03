1d5166c8eb00db3c8932d151ca8ce17def92f021
mkshkmrgpt <mkshkmrgpt@gmail.com>
2018-10-03 20:03:03 +0530

    add exception handler

--- src/main/kotlin/com/mukesh/webcrawler/CrawlerException.kt
+++ src/main/kotlin/com/mukesh/webcrawler/CrawlerException.kt
@@ -1,3 +1,24 @@
 package com.mukesh.webcrawler
 
-class CrawlerException(override val message: String?) :Exception(message)
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.context.request.WebRequest
+
+class CrawlerException(override val message: String?) : Exception(message)
+
+@ControllerAdvice
+class CrawlerExceptionHandler {
+
+    @ExceptionHandler(CrawlerException::class)
+    fun handleCrawlerException(crawlerException: CrawlerException, request: WebRequest): ResponseEntity<ApiError> {
+        return ResponseEntity(ApiError(crawlerException.message), HttpHeaders(), HttpStatus.BAD_REQUEST)
+    }
+
+    @ExceptionHandler
+    fun handleAllOtherException(exception: Exception, request: WebRequest): ResponseEntity<ApiError> {
+        return ResponseEntity(ApiError(exception.message), HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+}
